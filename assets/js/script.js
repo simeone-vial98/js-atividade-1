8675901e0c6fa6de36d3332912ce04b1cb8b7038
simeone-vial98 <82850332+simeone-vial98@users.noreply.github.com>
2021-05-15 21:45:13 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
-alert('Olá!');
+var original = document.getElementById('original');
+var modificado = document.getElementById('modificado');
+var contador = document.getElementById("contador_de_caracteres");
+var efeito = document.getElementById("efeito");
+
+original.addEventListener('input', (event) => {
+    var texto = event.target.value;
+
+    contador.innerHTML = texto.length;
+
+    if(efeito.value == 'uppercase') {
+        modificado.innerHTML = texto.toUpperCase();
+    } else if(efeito.value == 'lowercase') {
+        modificado.innerHTML = texto.toLowerCase();
+    }
+});
+
+efeito.addEventListener('change', () => {
+    var texto = original.value;
+
+    if(efeito.value == 'uppercase') {
+        modificado.innerHTML = texto.toUpperCase();
+    } else if(efeito.value == 'lowercase') {
+        modificado.innerHTML = texto.toLowerCase();
+    }
+})
